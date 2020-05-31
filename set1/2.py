e592b9d08dee66ed72b23a0e128fbb9ada75376b
pekare <carl@pekare.se>
2020-05-31 18:39:40 +0200

    challenge2

--- set1/2.py
+++ set1/2.py
@@ -9,8 +9,14 @@ def hex2b64(hexstr):
     encoded_data = base64.b64encode(data)
     return encoded_data
 
+def xor(data, key):
+    xor_data = bytearray()
+    for d, k in zip(binascii.unhexlify(data), binascii.unhexlify(key)):
+        xor_data.append(d ^ k)
+    return binascii.hexlify(xor_data)
+
 def main(argv):
-    print(hex2b64(argv[0]))
+    print(xor(argv[0],argv[1]))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
