85fb8a151e3da7c3e077f5c7b89e64bd5cce6eac
pekare <carl@pekare.se>
2020-06-02 23:07:28 +0200

    challenge3

--- set1/3.py
+++ set1/3.py
@@ -8,33 +8,34 @@ from itertools import cycle, islice
 from math import sqrt
 from collections import Counter
 
-english_letter_probability =
+english_letter_probability = {
     "a": 0.08497,
     "b": 0.01492,
-"c2.202
-d4.253
-e11.162
-f2.228
-g2.015
-h6.094
-i7.546
-j0.153
-k1.292
-l4.025
-m2.406
-n6.749
-o7.507
-p1.929
-q0.095
-r7.587
-s6.327
-t9.356
-u2.758
-v0.978
-w2.560
-x0.150
-y1.994
-z0.077
+    "c": 0.02202,
+    "d": 0.04253,
+    "e": 0.11162,
+    "f": 0.02228,
+    "g": 0.02015,
+    "h": 0.06094,
+    "i": 0.07546,
+    "j": 0.00153,
+    "k": 0.01292,
+    "l": 0.04025,
+    "m": 0.02406,
+    "n": 0.06749,
+    "o": 0.07507,
+    "p": 0.01929,
+    "q": 0.00095,
+    "r": 0.07587,
+    "s": 0.06327,
+    "t": 0.09356,
+    "u": 0.02758,
+    "v": 0.00978,
+    "w": 0.02560,
+    "x": 0.00150,
+    "y": 0.01994,
+    "z": 0.00077
+}
 
 # string in, bytes out
 def hex2b64(hexstr):
@@ -52,10 +53,10 @@ def xor(b_data, b_key):
 
 # Bhattacharyya Coefficient
 def score(string):
-    c = Counter(string.toLower())
+    c = Counter(string.lower())
     score = 0 
     for letter, letter_count in c.items():
-        string_probability = letter_count/lee(string)
+        string_probability = letter_count/len(string)
         english_probability = english_letter_probability.get(letter, 0) # no matches in dict => 0 in probability
         score += sqrt(string_probability * english_probability)
     return score
@@ -66,7 +67,7 @@ def main(argv):
         b_key = char.encode('utf-8')
         xor_data = xor(b_data, b_key)
         string = xor_data.decode('utf-8')
-        print(char + ": " + score(string) + " = " + )
+        print(f"char:{char} score:{score(string)} string:{string}")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
